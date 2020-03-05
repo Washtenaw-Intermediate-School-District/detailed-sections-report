2889043f1ca4875a0f03d27c59e1c39c972d857f
Solomon Zheng <58829220+wisd-solomon@users.noreply.github.com>
2020-03-05 16:16:16 -0500

    Update sectionListHMH.sql
    added the school to the columns as well and separated the term out for easier sorting

--- sectionListHMH.sql
+++ sectionListHMH.sql
@@ -1,12 +1,14 @@
 SELECT
 	teachers.ID AS teacher,
-    s.external_expression || '-' || c.course_name || '-(' || s.id || ')' || '-' || t.abbreviation "Class"
+    s.external_expression || '-' || c.course_name || '-(' || s.id || ')' "Class",
+    t.abbreviation,
+    sch.name
     
 FROM SECTIONS s
     LEFT JOIN(
         SELECT
             st.SECTIONID,
-            LISTAGG(u.LASTFIRST,', ') WITHIN GROUP (ORDER BY st.SECTIONID) "ID"
+            LISTAGG(u.LASTFIRST,'; ') WITHIN GROUP (ORDER BY st.SECTIONID) "ID"
         FROM SECTIONTEACHER st
             INNER JOIN SECTIONS s ON s.ID = st.SECTIONID
             INNER JOIN TERMS t ON t.ID = s.TERMID AND t.SCHOOLID = s.SCHOOLID
@@ -18,8 +20,9 @@ FROM SECTIONS s
     INNER JOIN TERMS t ON t.ID = s.TERMID AND t.SCHOOLID = s.SCHOOLID
     INNER JOIN SCHOOLSTAFF ss ON s.TEACHER = ss.ID AND s.SCHOOLID = ss.SCHOOLID
     INNER JOIN COURSES c ON s.course_number = c.course_number
+    INNER JOIN SCHOOLS sch ON s.schoolid = sch.school_number
 
 WHERE t.YEARID = EXTRACT(YEAR FROM SYSDATE) + CASE WHEN EXTRACT(MONTH FROM SYSDATE) >= 8 THEN 1 ELSE 0 END - 1991
-    AND s.schoolid = 9404
+
     
 ORDER BY t.abbreviation,teachers.ID, s.external_expression || '-' || c.course_name || '-(' || s.id || ')'
